5d7113bf5f14918949a157f35ad0416b300b56cb
Pedro Viegas <122245816+deluscc4@users.noreply.github.com>
2023-03-17 21:53:14 -0300

    Add files via upload

---
+++ pessoas_vis.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "banco.php";
+    $dados_clientes= obter_dados();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+<table class="table">
+    <thead class="thead-dark">
+    <tr>
+        <th scope="col">Nome</th>
+        <th scope="col">Idade</th>
+        <th scope="col">Estado</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php for ($i=0; $i<count($dados_clientes); $i++){ ?>
+    <tr>
+        <td><?php echo $dados_clientes[$i]['nome']; ?></td>
+        <td><?php echo $dados_clientes[$i]['idade']; ?></td>
+        <td><?php echo $dados_clientes[$i]['estado']; ?></td>
+        </tr>
+    <?php } ?>
+    </tbody></table></body></html>
